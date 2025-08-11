11c169f55837b6ae2d3036defe7a0fe05c9deb58
abdullah-mamun369 <maamamun369@gmail.com>
2025-08-11 17:34:49 +0600

    Origin updated

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app: Application = express();
 //parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "https://3dmodels.hice.com.au", credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 // application routes
 app.use("/api/v1", router);
